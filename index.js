28f1259640de030a5c0b2bdb21ade0427a79db3c
A-James-Johnson <jamesjohnson.a2022it@sece.ac.in>
2024-03-08 14:26:12 +0530

    bghjew

---
+++ index.js
@@ -0,0 +1,129 @@
+const express=require('express')
+const cors=require('cors')
+const mongoose=require('mongoose')
+const { Expense } = require('./schema.js')
+const bodyParser = require('body-parser')
+const app=express()
+app.use(bodyParser.json())
+app.use(cors())
+
+async function connectToDb(){
+    try{
+        await mongoose.connect('mongodb+srv://James321485:James321485@cluster0.835jjax.mongodb.net/ExpenseTracker?retryWrites=true&w=majority&appName=Cluster0')
+        console.log('DB connection established ;')
+
+        const port=process.env.PORT || 8000
+        app.listen(port,function(){
+            console.log(`Listening on port ${port}...`)
+        })
+    }catch(error){
+        console.log(error)
+        console.log('error')
+    }
+}
+connectToDb()
+
+app.get('/get-expenses', async function(request, response) {
+    try {
+        const expensesData = await Expense.find()
+        response.status(201).json(expensesData)
+    } catch(error) {
+        response.status(500).json({
+            "status" : "failure",
+            "message" : "could not fetch details",
+            "error":error
+        })
+    }
+})
+
+
+
+app.post('/add-expense',async function(request,response){
+   
+    try{
+        await Expense.create({
+            "amount":request.body.amount,
+            "category":request.body.category,
+            "date":request.body.date
+        })
+        response.status(201).json({
+            "status":"success",
+            "message":"entry created"
+        })
+    }
+    catch(error){
+        response.status(500).json({
+            "status":"unsuccess",
+            "message":"entry not created"
+        })
+    }
+})
+
+app.delete('/delete-expense/:id', async function(request, response) {
+    try {
+        const expenseEntry = await Expense.findById(request.params.id)
+        if(expenseEntry) {
+            await Expense.findByIdAndDelete(request.params.id)
+            response.status(200).json({
+                "status" : "success",
+                "message" : "successfully deleted the entry"
+            })
+        } else {
+            response.status(404).json({
+                "status" : "failure",
+                "message" : "could not find the entry"
+            })
+        }
+    } catch(error) {
+        response.status(500).json({
+            "status" : "failure",
+            "message" : "could not delete entry",
+            "error" : error
+        })
+    }
+})
+
+
+app.patch('/update-expense/:id', async function(request, response) {
+    try {
+        const expenseEntry = await Expense.findById(request.params.id)
+        if(expenseEntry) {
+            await expenseEntry.updateOne({
+                "amount" : request.body.amount,
+                "category" : request.body.category,
+                "date" : request.body.date
+            })
+            response.status(200).json({
+                "status" : "success",
+                "message" : "successfully updated the entry"
+            })
+        } else {
+            response.status(404).json({
+                "status" : "failure",
+                "message" : "could not find the entry"
+            })
+        }
+    } catch(error) {
+        response.status(500).json({
+            "status" : "failure",
+            "message" : "could not update entry",
+            "error" : error
+        })
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
